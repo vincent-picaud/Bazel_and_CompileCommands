21bc71ec1877f2409721d8053e55dfae3f8dcf88
picaud <vincent.picaud@laposte.net>
2017-06-26 16:46:41 +0200

    type + add category

--- create_compile_commands.sh
+++ create_compile_commands.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#!/bin/sh
-#
 set -e
 bazel build --experimental_action_listener=//tools/actions:generate_compile_commands_listener $1
 python3 ./tools/actions/generate_compile_commands_json.py
--- setup_compile_commands.sh
+++ setup_compile_commands.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# 
 set -e
 
 if [ ! -f "WORKSPACE" ]; then
