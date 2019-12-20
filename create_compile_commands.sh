a452de1b9f2229f3d32a302fd41851d578b149af
tangxinfa <tangxinfa@gmail.com>
2019-12-20 10:08:47 +0800

    fix: support pass bazel build arguments.

--- create_compile_commands.sh
+++ create_compile_commands.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $(basename $0) BAZEL_TARGET"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $(basename $0) BAZEL_BUILD_ARGUMENTS"
     exit 1
 fi
 
-bazel build --experimental_action_listener=//tools/actions:generate_compile_commands_listener $1
+bazel build --experimental_action_listener=//tools/actions:generate_compile_commands_listener $*
 python3 ./tools/actions/generate_compile_commands_json.py
 exit 0
